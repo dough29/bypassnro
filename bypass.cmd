d8903aa956f2fcfec0dcbf8bde905e7b04b1c88d
Damien alias Dough <dough29@gmail.com>
2025-11-28 17:32:15 +0100

    Update bypass.cmd

--- bypass.cmd
+++ bypass.cmd
@@ -1,2 +1,2 @@
-curl -L -o C:\Windows\Panther\unattend.xml https://raw.githubusercontent.com/ChrisTitusTech/bypassnro/refs/heads/main/unattend.xml
+curl -L -o C:\Windows\Panther\unattend.xml https://github.com/dough29/bypassnro/raw/refs/heads/damien/unattend.xml
 %WINDIR%\System32\Sysprep\Sysprep.exe /oobe /unattend:C:\Windows\Panther\unattend.xml /reboot
